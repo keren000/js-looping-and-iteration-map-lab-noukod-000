093da4fe8f1f6507a624fc13eb34319777ab4617
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-21 15:32:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // Code your solution in this file.
-function lowerCaseDrivers(array){
-    return array.map(function(driver){
-      return driver.toLowerCase();
-    });
+function lowerCaseDrivers(array)
+{
+    return array.map(function(driver){return driver.toLowerCase();});
 }
 
 function nameToAttributes(profile_user){
